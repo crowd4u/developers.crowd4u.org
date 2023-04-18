ee0b8064fddabc38296c885e78f956ea981b1b90
Nakaya <eniehack@outlook.jp>
2023-04-18 11:08:38 +0900

    mv dir

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,10 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
 }
