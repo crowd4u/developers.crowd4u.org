caa1dc762f7f7a9b4053d33784785c4924adc91d
Nakaya <eniehack@outlook.jp>
2023-04-18 15:38:52 +0900

    画像ファイルをcopyするようにした

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    eleventyConfig.addPassthroughCopy("**/*.png");
     return {
         dir: {
             input: "src",
