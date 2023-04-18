02eadd8628a786269d199af82176b765c5e21a90
Nakaya <eniehack@outlook.jp>
2023-04-18 16:20:17 +0900

    タグの実装

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,19 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     eleventyConfig.addPassthroughCopy("**/*.png");
+
+    eleventyConfig.addFilter("getAllTags", collection => {
+        let tagset = new Set();
+        collection.forEach(post => {
+            (post.data.tags || []).forEach(tag => tagset.add(tag))
+        });
+        return Array.from(tagset);
+    });
+
+    eleventyConfig.addFilter("filterTagList", tags => {
+        return (tags || []).filter(tag => ["posts"].indexOf(tag) === -1);
+    })
+
     return {
         dir: {
             input: "src",
