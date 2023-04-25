ed756ff3c9ddf872547cedb32ae6808a277f6a8c
Nakaya <eniehack@outlook.jp>
2023-04-25 16:19:27 +0900

    著者リンク、タグリンクを作成するShortcodeを作成

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-    eleventyConfig.addPassthroughCopy("**/*.png");
+
+    eleventyConfig.addPassthroughCopy("posts/**/*.png");
 
     eleventyConfig.addFilter("getAllTags", collection => {
         let tagset = new Set();
@@ -14,9 +15,24 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addFilter("filterTagList", tags => {
         return (tags || []).filter(tag => ["posts"].indexOf(tag) === -1);
-    })
+    });
+
+    eleventyConfig.addFilter("filterAuthor", (collections, author) => {
+        collections.filter(post => post.data.author === author)
+    });
+
+    eleventyConfig.addShortcode("author_link", (id, text) => {
+        let path = "/author/" + eleventyConfig.getFilter("slugify")(id);
+        return `<a href="${eleventyConfig.getFilter("htmlBaseUrl")(path)}">${text}</a>`;
+    });
+
+    eleventyConfig.addShortcode("tag_link", (tag) => {
+        let path = "/tag/" + eleventyConfig.getFilter("slugify")(tag);
+        return `<a href="${eleventyConfig.getFilter("htmlBaseUrl")(path)}">${tag}</a>`;
+    });
 
     return {
+        markdownTemplateEngine: "njk",
         dir: {
             input: "src",
             output: "public"
